1e289091d2563074cd21a0ff7ebff8bbb0563f03
antoniomrrds <arqueiroverd@gmail.com>
2022-01-23 04:38:29 -0300

    Projeto Pronto com async e await

--- products/ProductsController.js
+++ products/ProductsController.js
@@ -5,50 +5,45 @@ const validation = require('../validation/validation');
 const { Op } = require('sequelize');
 const { empty } = validation()
 
-//read
-router.get('/products', (req, res) => {
-    Products.findAll()
-        .then(data => {
-            if (data == undefined) {
-                res.sendStatus(204)
-            } else {
-                res.statusCode = 200;
-                res.json(data)
-            }
-        }).catch(err => {
-            res.sendStatus(404);
-        })
-
+//buscar todos os produtos
+router.get('/products', async (req, res) => {
+    try {
+        const product = await Products.findAll();
+       
+        if (product == null || product.length == 0 ) {
+            res.sendStatus(404)
+        } else {
+            res.statusCode = 200;
+            res.json(product)
+        }
+    } catch (err) {
+        res.sendStatus(500);
+    }
 })
 
-//read by iu
-
-router.get('/product/:id', (req, res) => {
+//Buscar produto por id
+router.get('/product/:id', async (req, res) => {
     const id = req.params.id
     if (isNaN(id)) {
         res.sendStatus(400)
     } else {
-        Products.findOne({
-            where: {
-                id: id
-            }
-        }).then(data => {
-            if (data == null) {
-                res.sendStatus(204)
+        try {
+            const productId = await Products.findOne({ where: { id: id } })
+        
+            if (productId == null) {
+                res.sendStatus(404)
             } else {
                 res.statusCode = 200;
-                res.json(data);
-
+                res.json(productId);
             }
-        }
-        ).catch(err => {
+        } catch (err) {
             res.sendStatus(500)
-        })
+        }
     }
 })
 
-//create
-router.post('/product', (req, res) => {
+//Adicionar Produto
+router.post('/product', async (req, res) => {
     const { nome_produto, fabricante, quantidade_estoque, valor } = req.body;
 
     const product = {
@@ -62,196 +57,151 @@ router.post('/product', (req, res) => {
     if (productData) {
         const dataProduct = keysProduct.some(el => empty(product[el]))
         if (dataProduct) {
-            res.sendStatus(400)
+            res.sendStatus(400);
         } else {
-            Products.create({
-                nome_produto: nome_produto,
-                fabricante: fabricante,
-                quantidade_estoque: quantidade_estoque,
-                valor: valor
-
-            })
-                .then(_ => res.sendStatus(201))
-                .catch(err => {
-                    res.sendStatus(500)
+            try {
+                const product = await Products.create({
+                    nome_produto: nome_produto,
+                    fabricante: fabricante,
+                    quantidade_estoque: quantidade_estoque,
+                    valor: valor
                 })
-
+                res.sendStatus(201)
+            } catch (err) {
+                res.sendStatus(500)
+            }
         }
     } else {
         res.sendStatus(400)
     }
-
 })
 
-//update
-router.put('/product/:id', (req, res) => {
+//edição de Produto
+router.put('/product/:id', async (req, res) => {
     const id = req.params.id
 
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
 
-        Products.findOne({
-            where: {
-                id: id
+        const productId = await Products.findOne({ where: { id: id } })
+
+        if (productId == null) {
+            res.sendStatus(404);
+        } else {
+            const { nome_produto, fabricante, quantidade_estoque, valor } = req.body;
+
+            const product = {
+                nome_produto: nome_produto,
+                fabricante: fabricante,
+                quantidade_estoque: quantidade_estoque,
+                valor: valor
             }
-        })
-            .then(data => {
-                if (data == null) {
-                    res.sendStatus(204);
+
+            const keysProduct = Object.keys(product)
+            const productNotUndefined = keysProduct.every(data => product[data] != undefined);
+            if (productNotUndefined) {
+                const productEmpty = keysProduct.some(el => empty(product[el]))
+                if (productEmpty) {
+                    res.sendStatus(400)
                 } else {
-                    const { nome_produto, fabricante, quantidade_estoque, valor } = req.body;
-
-                    const product = {
-                        nome_produto: nome_produto,
-                        fabricante: fabricante,
-                        quantidade_estoque: quantidade_estoque,
-                        valor: valor
-                    }
-                    const keysProduct = Object.keys(product)
-                    const productData = keysProduct.every(data => product[data] != undefined);
-                    if (productData) {
-                        const dataProduct = keysProduct.some(el => empty(product[el]))
-                        if (dataProduct) {
-                            res.sendStatus(400)
-                        } else {
-
-                            Products.update({
-                                nome_produto: nome_produto,
-                                fabricante: fabricante,
-                                quantidade_estoque: quantidade_estoque,
-                                valor: valor
-
-                            }, {
-                                where: {
-                                    id: id
-
-                                }
-                            })
-                                .then(_ => {
-                                    res.sendStatus(200);
-                                })
-                                .catch(_ => res.sendStatus(500))
-
-                        }
-                    } else {
-                        res.sendStatus(400)
+                    try {
+                        const editProduct = await Products.update({
+                            nome_produto: nome_produto,
+                            fabricante: fabricante,
+                            quantidade_estoque: quantidade_estoque,
+                            valor: valor
+                        }, { where: { id: id } });
+                        res.sendStatus(200);
+                    } catch (err) {
+                        res.sendStatus(500)
                     }
                 }
-            })
-
-            .catch(_ => res.sendStatus(500))
+            } else {
+                res.sendStatus(400)
+            }
+        }
 
     }
+
 })
 
-//delete
-router.delete('/product/:id', (req, res) => {
+//apagar produto
+router.delete('/product/:id', async (req, res) => {
     const id = req.params.id
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
-        Products.destroy({
-            where: {
-                id: id
+        try {
+            const productId = await Products.findOne({ where: { id: id } })
+            if (productId == null) {
+                res.sendStatus(404)
+            } else {
+                const productDelete = await Products.destroy({ where: { id: id } })
+                res.sendStatus(200);
             }
-        })
-            .then(_ => res.sendStatus(200))
-            .catch(_ => res.sendStatus(500))
+        } catch (err) {
+            res.sendStatus(500)
+        }
     }
 })
 
-
-// total products
-
-router.get('/products/theAmount',async (req, res) => {
-    try{
+// total produtos
+router.get('/products/theAmount', async (req, res) => {
+    try {
         const theAmount = await Products.findAndCountAll();
-            res.statusCode = 200;
-            res.json(theAmount.count)
-    }catch(err){
+        res.statusCode = 200;
+        res.json(theAmount.count)
+    } catch (err) {
         res.sendStatus(500)
-    } 
-})
-
-//
-router.get('/products/lowerStock', async (req,res) =>{
-    try{
-        const lower = await Products.min('quantidade_estoque');
-        console.log(lower)
-        const stock = await Products.findOne({
-            where:{
-                quantidade_estoque:lower
-            }
-        })
-        if(stock == null){
-            res.sendStatus(204)
-        }else{
-            res.statusCode = 200;
-            res.send(stock)
-        }
-      
-    }catch(err){
-         res.sendStatus(500)
     }
 })
 
-
-
-router.get('/products/lowerStock', async (req,res) =>{
-    try{
-        const lower = await Products.min('quantidade_estoque');
-        const stock = await Products.findOne({
-            where:{
-                quantidade_estoque:lower
-            }
-        })
-        if(stock == null){
-            res.sendStatus(204)
-        }else{
+//menor estoque
+router.get('/products/lowerStock', async (req, res) => {
+    try {
+        const lower = await Products.min('quantidade_estoque')
+        const stock = await Products.findOne({ where: { quantidade_estoque: lower } })
+        if (stock == null) {
+            res.sendStatus(404)
+        } else {
             res.statusCode = 200;
             res.send(stock)
         }
-      
-    }catch(err){
-         res.sendStatus(500)
+
+    } catch (err) {
+        res.sendStatus(500)
     }
 })
 
-
-router.get('/products/biggerStock', async (req,res) =>{
-    try{
+//maior estoque
+router.get('/products/biggerStock', async (req, res) => {
+    try {
         const bigger = await Products.max('quantidade_estoque');
-        const stock = await Products.findOne({
-            where:{
-                quantidade_estoque:bigger
-            }
-        })
-        if(stock == null){
-            res.sendStatus(204)
-        }else{
+        const stock = await Products.findOne({ where: { quantidade_estoque: bigger } })
+        if (stock == null) {
+            res.sendStatus(404)
+        } else {
             res.statusCode = 200;
             res.send(stock)
         }
-      
-    }catch(err){
-         res.sendStatus(500)
-        }
-    })
-    
-    router.get('/products/withoutStock', async (req,res)=>{
-        
-        try{
-            const withoutStock = await Products.findAll({
-                where: {
-                    quantidade_estoque: {[Op.lt]:5}
-                }
-            })
+    } catch (err) {
+        res.sendStatus(500)
+    }
+})
+
+//sem estoque 
+router.get('/products/withoutStock', async (req, res) => {
+    try {
+        const withoutStock = await Products.findAll({where: {quantidade_estoque: { [Op.lt]: 5 } }})
+        if(withoutStock == null || withoutStock.length == 0){
+            res.sendStatus(404)
+        }else{
             res.statusCode = 200;
             res.json(withoutStock)
-
-        }catch(err){
-            res.sendStatus(500)
+        }
+    } catch (err) {
+        res.sendStatus(500)
     }
 
 })
